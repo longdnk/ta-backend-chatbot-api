6b4b70a458e88eb5cc5a161042a5d374f2a6f8ba
kimlong <kimlong101020@gmail.com>
2025-02-15 21:41:26 +0700

    add update version

--- schema/question_rewriter.py
+++ schema/question_rewriter.py
@@ -7,7 +7,6 @@ from langchain_core.output_parsers import StrOutputParser
 re_write_prompt = PromptTemplate(
     template="""You a question re-writer that converts an input question to a better version that is optimized \n 
      for vectorstore retrieval. Look at the initial and formulate an improved question. \n
-     Hint: If the original question asks "momo ra đời năm nào", transform it to "momo được thành lập năm nào" for clarity.
      Here is the initial question: \n\n {question}. Improved question with no preamble: \n 
     """,
     input_variables=["generation", "question"],
